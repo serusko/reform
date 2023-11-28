c8a06b774608e4eea9ff126e55dedc533f9dfbe0
serusko <hruzmatus@gmail.com>
2023-11-28 20:05:20 +0100

    exclude react from build

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,20 @@
 import { resolve } from 'path';
 
 import react from '@vitejs/plugin-react';
+import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 import eslint from 'vite-plugin-eslint';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     copyPublicDir: false,
     lib: { entry: resolve(__dirname, 'lib/index.ts'), formats: ['es'] },
     rollupOptions: {
-      external: ['react', 'react/jsx-runtime'],
+      external: ['react', 'react/jsx-runtime', 'react-dom'],
       output: { entryFileNames: 'index.js' },
     },
     sourcemap: true,
   },
-  plugins: [react(), eslint(), dts({ copyDtsFiles: true, include: ['lib'] })],
+  plugins: [visualizer(), react(), eslint(), dts({ copyDtsFiles: true, include: ['lib'] })],
 });
